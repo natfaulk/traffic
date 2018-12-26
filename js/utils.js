a6251f2b5e8b7663a4551229461504ced0c6fbb8
natfaulk <nfaulk@hotmail.co.uk>
2018-12-26 23:11:01 +0000

    make cars more identifiable

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+module.exports = {
+  randomColour: () => {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return [r,g,b]
+  }
+}
--- js/vehicle.js
+++ js/vehicle.js
@@ -1,18 +1,20 @@
 const Point = require('./point')
 const Settings = require('./settings')
+const Utils = require('./utils')
 
 module.exports = class Vehicle {
   constructor(_x = 0, _y = 0) {
     this.pos = new Point(_x, _y)
     this.angle = 0
-    this.size = new Point(10, 5)
-    this.waypoints = []    
+    this.size = new Point(Settings.CAR_LENGTH, Settings.CAR_WIDTH)
+    this.waypoints = []
+    this.color = Utils.randomColour()
   }
 
   draw(_d) {
     _d.strokeWeight(1)
     _d.stroke('black')
-    _d.fill('red')
+    _d.fill(`rgb(${this.color[0]},${this.color[1]},${this.color[2]})`)
     _d.rotatedRect(this.pos.x + 0.5, this.pos.y + 0.5, this.size.x, this.size.y, this.angle)
     if (this.waypoints.length > 0) {
       _d.fill('green')
