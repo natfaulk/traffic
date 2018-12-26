ab51a6efbc8f7c0a21038a6dc9cde1c75acbb54c
natfaulk <nfaulk@hotmail.co.uk>
2018-12-26 22:26:52 +0000

    angle cars towards destination

--- js/settings.js
+++ js/settings.js
@@ -2,5 +2,6 @@ module.exports = {
   LANE_WIDTH: 15,
   DEST_THRESHOLD: 0.05,
   GRID_SIZE: 25,
-  MAX_WAYPOINTS: 20
+  MAX_WAYPOINTS: 20,
+  MAX_SPEED: 0.5
 }
--- js/vehicle.js
+++ js/vehicle.js
@@ -4,17 +4,16 @@ const Settings = require('./settings')
 module.exports = class Vehicle {
   constructor(_x = 0, _y = 0) {
     this.pos = new Point(_x, _y)
+    this.angle = 0
     this.size = new Point(10, 5)
-    this.waypoints = []
-    
-    this.MAX_SPEED = 1.5
+    this.waypoints = []    
   }
 
   draw(_d) {
     _d.strokeWeight(1)
     _d.stroke('black')
     _d.fill('red')
-    _d.rect(this.pos.x + 0.5, this.pos.y + 0.5, this.size.x, this.size.y)
+    _d.rotatedRect(this.pos.x + 0.5, this.pos.y + 0.5, this.size.x, this.size.y, this.angle)
     if (this.waypoints.length > 0) {
       _d.fill('green')
       _d.ellipse(this.waypoints[0].x, this.waypoints[0].y, 5)
@@ -35,10 +34,11 @@ module.exports = class Vehicle {
       let d = Point.distance(this.pos, dest)
       if (d > Settings.DEST_THRESHOLD) {
         let a = Point.angle(dest, this.pos)
-        if (d > this.MAX_SPEED) d = this.MAX_SPEED
+        if (d > Settings.MAX_SPEED) d = Settings.MAX_SPEED
         let t = new Point()
         t.setFromPolar(a, d)
         this.pos.add(t)
+        this.angle = a
       } else {
         this.waypoints.shift()
         this.tick()
